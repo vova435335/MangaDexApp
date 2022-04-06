1086ca7ec0fd9208ba88f513980f39c472f441e7
vova4 <435335vova@gmail.com>
2022-04-06 04:28:28 +0300

    Refactoring

--- app/src/main/java/ru/vld43/mangadexapp/ui/MainActivity.kt
+++ app/src/main/java/ru/vld43/mangadexapp/ui/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.vld43.mangadexapp.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
