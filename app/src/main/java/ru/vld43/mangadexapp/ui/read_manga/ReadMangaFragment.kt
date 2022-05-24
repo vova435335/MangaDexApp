a31177bcf88da40670a51f3224dfe3309461bd7e
vova4 <435335vova@gmail.com>
2022-05-24 09:34:01 +0300

    Вёрстка

---
+++ app/src/main/java/ru/vld43/mangadexapp/ui/read_manga/ReadMangaFragment.kt
@@ -0,0 +1,7 @@
+package ru.vld43.mangadexapp.ui.read_manga
+
+import androidx.fragment.app.Fragment
+import ru.vld43.mangadexapp.R
+
+class ReadMangaFragment: Fragment(R.layout.fragment_read_manga) {
+}
